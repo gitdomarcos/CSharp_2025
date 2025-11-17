627b6dcb88aea2424ed4e777aef63149ca80f69f
Marcos Vinicius <marcao_@live.com>
2025-11-17 15:07:41 -0300

    Front Exercicio 1 XML - Cadastro Veiculos

---
+++ frmXml1.Designer.cs
@@ -0,0 +1,369 @@
+﻿namespace CSharp_2025
+{
+    partial class frmXml1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbXmlEx1 = new System.Windows.Forms.GroupBox();
+            this.lblCodigo = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblMarca = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lblModelo = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.lblAnoFab = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.lblValorVenda = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.lblPlaca = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.lblQntdPortas = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.lblCombustivel = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.lblValorCompra = new System.Windows.Forms.Label();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnCadastrar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnAlterar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.gbXmlEx1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gbXmlEx1
+            // 
+            this.gbXmlEx1.Controls.Add(this.label1);
+            this.gbXmlEx1.Controls.Add(this.btnExcluir);
+            this.gbXmlEx1.Controls.Add(this.btnAlterar);
+            this.gbXmlEx1.Controls.Add(this.btnCancelar);
+            this.gbXmlEx1.Controls.Add(this.btnCadastrar);
+            this.gbXmlEx1.Controls.Add(this.dataGridView1);
+            this.gbXmlEx1.Controls.Add(this.textBox9);
+            this.gbXmlEx1.Controls.Add(this.lblValorCompra);
+            this.gbXmlEx1.Controls.Add(this.textBox8);
+            this.gbXmlEx1.Controls.Add(this.lblCombustivel);
+            this.gbXmlEx1.Controls.Add(this.textBox7);
+            this.gbXmlEx1.Controls.Add(this.lblQntdPortas);
+            this.gbXmlEx1.Controls.Add(this.textBox6);
+            this.gbXmlEx1.Controls.Add(this.lblPlaca);
+            this.gbXmlEx1.Controls.Add(this.textBox5);
+            this.gbXmlEx1.Controls.Add(this.lblValorVenda);
+            this.gbXmlEx1.Controls.Add(this.textBox4);
+            this.gbXmlEx1.Controls.Add(this.lblAnoFab);
+            this.gbXmlEx1.Controls.Add(this.textBox3);
+            this.gbXmlEx1.Controls.Add(this.lblModelo);
+            this.gbXmlEx1.Controls.Add(this.textBox2);
+            this.gbXmlEx1.Controls.Add(this.lblMarca);
+            this.gbXmlEx1.Controls.Add(this.textBox1);
+            this.gbXmlEx1.Controls.Add(this.lblCodigo);
+            this.gbXmlEx1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbXmlEx1.Location = new System.Drawing.Point(12, 12);
+            this.gbXmlEx1.Name = "gbXmlEx1";
+            this.gbXmlEx1.Size = new System.Drawing.Size(1244, 755);
+            this.gbXmlEx1.TabIndex = 0;
+            this.gbXmlEx1.TabStop = false;
+            this.gbXmlEx1.Text = "XML - Exercício 01";
+            // 
+            // lblCodigo
+            // 
+            this.lblCodigo.AutoSize = true;
+            this.lblCodigo.Location = new System.Drawing.Point(115, 107);
+            this.lblCodigo.Name = "lblCodigo";
+            this.lblCodigo.Size = new System.Drawing.Size(98, 29);
+            this.lblCodigo.TabIndex = 0;
+            this.lblCodigo.Text = "Código:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(228, 104);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(191, 34);
+            this.textBox1.TabIndex = 1;
+            // 
+            // lblMarca
+            // 
+            this.lblMarca.AutoSize = true;
+            this.lblMarca.Location = new System.Drawing.Point(128, 169);
+            this.lblMarca.Name = "lblMarca";
+            this.lblMarca.Size = new System.Drawing.Size(85, 29);
+            this.lblMarca.TabIndex = 0;
+            this.lblMarca.Text = "Marca:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(228, 166);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(191, 34);
+            this.textBox2.TabIndex = 1;
+            // 
+            // lblModelo
+            // 
+            this.lblModelo.AutoSize = true;
+            this.lblModelo.Location = new System.Drawing.Point(112, 234);
+            this.lblModelo.Name = "lblModelo";
+            this.lblModelo.Size = new System.Drawing.Size(101, 29);
+            this.lblModelo.TabIndex = 0;
+            this.lblModelo.Text = "Modelo:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(228, 231);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(191, 34);
+            this.textBox3.TabIndex = 1;
+            // 
+            // lblAnoFab
+            // 
+            this.lblAnoFab.AutoSize = true;
+            this.lblAnoFab.Location = new System.Drawing.Point(26, 304);
+            this.lblAnoFab.Name = "lblAnoFab";
+            this.lblAnoFab.Size = new System.Drawing.Size(187, 29);
+            this.lblAnoFab.TabIndex = 0;
+            this.lblAnoFab.Text = "Ano Fabricação:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(228, 301);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(191, 34);
+            this.textBox4.TabIndex = 1;
+            // 
+            // lblValorVenda
+            // 
+            this.lblValorVenda.AutoSize = true;
+            this.lblValorVenda.Location = new System.Drawing.Point(441, 304);
+            this.lblValorVenda.Name = "lblValorVenda";
+            this.lblValorVenda.Size = new System.Drawing.Size(150, 29);
+            this.lblValorVenda.TabIndex = 0;
+            this.lblValorVenda.Text = "Valor Venda:";
+            // 
+            // textBox5
+            // 
+            this.textBox5.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox5.Location = new System.Drawing.Point(613, 301);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(191, 34);
+            this.textBox5.TabIndex = 1;
+            // 
+            // lblPlaca
+            // 
+            this.lblPlaca.AutoSize = true;
+            this.lblPlaca.Location = new System.Drawing.Point(134, 376);
+            this.lblPlaca.Name = "lblPlaca";
+            this.lblPlaca.Size = new System.Drawing.Size(79, 29);
+            this.lblPlaca.TabIndex = 0;
+            this.lblPlaca.Text = "Placa:";
+            // 
+            // textBox6
+            // 
+            this.textBox6.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox6.Location = new System.Drawing.Point(228, 373);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(191, 34);
+            this.textBox6.TabIndex = 1;
+            // 
+            // lblQntdPortas
+            // 
+            this.lblQntdPortas.AutoSize = true;
+            this.lblQntdPortas.Location = new System.Drawing.Point(441, 107);
+            this.lblQntdPortas.Name = "lblQntdPortas";
+            this.lblQntdPortas.Size = new System.Drawing.Size(146, 29);
+            this.lblQntdPortas.TabIndex = 0;
+            this.lblQntdPortas.Text = "Qntd Portas:";
+            // 
+            // textBox7
+            // 
+            this.textBox7.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.textBox7.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox7.Location = new System.Drawing.Point(613, 104);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(191, 34);
+            this.textBox7.TabIndex = 1;
+            // 
+            // lblCombustivel
+            // 
+            this.lblCombustivel.AutoSize = true;
+            this.lblCombustivel.Location = new System.Drawing.Point(441, 171);
+            this.lblCombustivel.Name = "lblCombustivel";
+            this.lblCombustivel.Size = new System.Drawing.Size(152, 29);
+            this.lblCombustivel.TabIndex = 0;
+            this.lblCombustivel.Text = "Combustível:";
+            // 
+            // textBox8
+            // 
+            this.textBox8.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.textBox8.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox8.Location = new System.Drawing.Point(613, 164);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(191, 34);
+            this.textBox8.TabIndex = 1;
+            // 
+            // lblValorCompra
+            // 
+            this.lblValorCompra.AutoSize = true;
+            this.lblValorCompra.Location = new System.Drawing.Point(441, 232);
+            this.lblValorCompra.Name = "lblValorCompra";
+            this.lblValorCompra.Size = new System.Drawing.Size(167, 29);
+            this.lblValorCompra.TabIndex = 0;
+            this.lblValorCompra.Text = "Valor Compra:";
+            // 
+            // textBox9
+            // 
+            this.textBox9.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.textBox9.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox9.Location = new System.Drawing.Point(613, 229);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(191, 34);
+            this.textBox9.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 471);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1232, 278);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCadastrar.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnCadastrar.Location = new System.Drawing.Point(438, 380);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(171, 56);
+            this.btnCadastrar.TabIndex = 3;
+            this.btnCadastrar.Text = "Cadastrar";
+            this.btnCadastrar.UseVisualStyleBackColor = true;
+            this.btnCadastrar.Click += new System.EventHandler(this.btnCadastrar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCancelar.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnCancelar.Location = new System.Drawing.Point(622, 380);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(171, 56);
+            this.btnCancelar.TabIndex = 3;
+            this.btnCancelar.Text = "Carncelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnAlterar
+            // 
+            this.btnAlterar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAlterar.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnAlterar.Location = new System.Drawing.Point(806, 380);
+            this.btnAlterar.Name = "btnAlterar";
+            this.btnAlterar.Size = new System.Drawing.Size(171, 56);
+            this.btnAlterar.TabIndex = 3;
+            this.btnAlterar.Text = "Alterar";
+            this.btnAlterar.UseVisualStyleBackColor = true;
+            this.btnAlterar.Click += new System.EventHandler(this.btnAlterar_Click);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnExcluir.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnExcluir.Location = new System.Drawing.Point(990, 380);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(171, 56);
+            this.btnExcluir.TabIndex = 3;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Firebrick;
+            this.label1.Location = new System.Drawing.Point(431, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(294, 39);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Cadastro Veículos";
+            // 
+            // frmXml1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1267, 779);
+            this.Controls.Add(this.gbXmlEx1);
+            this.Name = "frmXml1";
+            this.Text = "frmXml1";
+            this.Load += new System.EventHandler(this.frmXml1_Load);
+            this.gbXmlEx1.ResumeLayout(false);
+            this.gbXmlEx1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbXmlEx1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Label lblValorCompra;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label lblCombustivel;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label lblQntdPortas;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label lblPlaca;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label lblValorVenda;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label lblAnoFab;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label lblModelo;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lblMarca;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblCodigo;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnAlterar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnCadastrar;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ frmXml1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharp_2025
+{
+    public partial class frmXml1 : Form
+    {
+        public frmXml1()
+        {
+            InitializeComponent();
+        }
+
+        private void frmXml1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAlterar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
